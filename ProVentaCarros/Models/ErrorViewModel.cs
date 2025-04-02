fd6e200ef1d35628dafcc822d39bce3396d6d49f
Ivan Rosa Sandoval <User@WIN-JBH2C2BUVPP>
2025-04-02 14:21:35 -0600

    Add project files.

---
+++ ProVentaCarros/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProVentaCarros.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
